4440772314a6fd7b70db6f306c42dfa2643b00bd
Uroš Petrović <urospetrovic2070@gmail.com>
2026-05-03 14:19:34 +0200

    Header and footer completed

---
+++ src/lib/components/layout/Footer.svelte
@@ -0,0 +1,5 @@
+<footer class="bg-black/30 backdrop-blur-xl py-5">
+	<div class="mx-auto flex max-w-6xl items-center justify-center px-3 lg:px-0">
+		<p>API provided by Open-Meteo</p>
+	</div>
+</footer>
--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,11 +1,38 @@
 <script lang="ts">
-	import Search from './Search.svelte';
+	import SearchIcon from '$lib/icons/SearchIcon.svelte';
+	import { globalState } from '$lib/state/global.svelte';
+	import { weatherState } from '$lib/state/weather.svelte';
+
+	let width = $state(0);
 </script>
 
-<header class="bg-orange-500 py-5">
-	<nav class="mx-auto flex max-w-7xl items-center justify-between">
-		<p>Weather App Placeholders</p>
-		<Search />
-		<p>Selected Location</p>
-	</nav>
+<svelte:window bind:innerWidth={width} />
+
+<header class="bg-black/30 py-5 backdrop-blur-xl">
+	<div class="mx-auto flex max-w-6xl items-center px-3 lg:px-0">
+		<h1 class="flex-1 text-3xl">Tropocast</h1>
+		{#if width < 1024}
+			<button
+				onclick={() => {
+					globalState.global.modal.open = true;
+					document.body.style.overflow = 'hidden';
+				}}
+				class="cursor-pointer text-3xl"
+			>
+				<SearchIcon />
+			</button>
+		{:else}
+			<input
+				class="w-75 rounded-sm bg-black/30 p-3 backdrop-blur-xl focus:outline-none lg:w-100"
+				onfocus={() => {
+					globalState.global.modal.open = true;
+					document.body.style.overflow = 'hidden';
+				}}
+				placeholder="Search"
+			/>
+			<p class="flex-1 text-right">
+				{weatherState.weather?.ready ? weatherState.weather.name : 'Select Location'}
+			</p>
+		{/if}
+	</div>
 </header>
