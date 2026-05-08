b27c3ab3149dec04a4fe7c2b8591160f566a0d24
Uroš Petrović <urospetrovic2070@gmail.com>
2026-05-08 11:38:00 +0200

    Added 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<div class="mx-auto mt-15 max-w-100 rounded-sm bg-black/60 py-5 text-center backdrop-blur-xl">
+	<h1 class="mb-5 text-3xl md:text-4xl">404 - Page Not Found</h1>
+	<p class="text-md mb-2 md:text-lg">The page you are looking for does not exist.</p>
+	<p class="text-md md:text-lg">
+		Please click <a class="font-medium underline" href="/">here</a> to return to the home page.
+	</p>
+</div>
