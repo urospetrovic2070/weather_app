08db7df4739b6597499ab28196834a602d591afb
Uroš Petrović <urospetrovic2070@gmail.com>
2026-05-08 11:54:41 +0200

    Adjusted the maximum width of the error message box for the smaller screens

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,4 +1,4 @@
-<div class="mx-auto mt-15 max-w-100 rounded-sm bg-black/60 py-5 text-center backdrop-blur-xl">
+<div class="mx-auto mt-15 max-w-85 md:max-w-100 rounded-sm bg-black/60 py-5 text-center backdrop-blur-xl">
 	<h1 class="mb-5 text-3xl md:text-4xl">404 - Page Not Found</h1>
 	<p class="text-md mb-2 md:text-lg">The page you are looking for does not exist.</p>
 	<p class="text-md md:text-lg">
